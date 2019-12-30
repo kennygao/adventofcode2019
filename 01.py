dc11a6b6c5c5aed4d105090df0af496852cd43dd
kennygao <kennygao@users.noreply.github.com>
2019-12-29 20:00:00 -0800

    Day 01 Part 2.

--- 01.py
+++ 01.py
@@ -2,5 +2,12 @@ def fuel_required(mass):
     return mass // 3 - 2
 
 
+def recursive_fuel_required(mass):
+    fuel = fuel_required(mass)
+    return fuel + recursive_fuel_required(fuel) if fuel > 0 else 0
+
+
 with open('01.txt') as f:
-    print(sum(fuel_required(int(mass)) for mass in f))
+    modules = list(f)
+    print(f'Part 1: {sum(fuel_required(int(mass)) for mass in modules)}')
+    print(f'Part 2: {sum(recursive_fuel_required(int(mass)) for mass in modules)}')
